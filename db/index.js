3a4b4fcb36aab3c36dcfcd97291d4bdaf7f82263
ericpkatz <ericpkatz@gmail.com>
2023-02-01 12:34:54 -0500

    added getUsers

--- db/index.js
+++ db/index.js
@@ -2,6 +2,29 @@ const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/wiz_news_db');
 
 
+const getUsers = async()=> {
+  const SQL = `
+    SELECT users.id, users.name, stories.id as "storyId", stories.name as "storyName"
+    FROM stories
+    RIGHT JOIN users
+    ON stories."userId" = users.id
+  `;
+  const response = await client.query(SQL);
+  const data = {
+  };
+  response.rows.forEach( row => {
+    if(!data[row.id]){
+      data[row.id] = { id: row.id, name: row.name, stories: []};
+    }
+    if(row.storyId){
+      data[row.id].stories.push({
+        id: row.storyId,
+        name: row.storyName
+      });
+    }
+  });
+  return Object.values(data);
+};
 const createUser = async({ name })=> {
   const SQL = `
 INSERT INTO users(name)
@@ -44,10 +67,13 @@ const seedData = async()=> {
   const larry = await createUser({ name: 'larry'});
   const story = await createStory({ userId: moe.id, name: 'I love SQL'});
   const story2 = await createStory({ userId: moe.id, name: 'I love SQL!'});
+  const users = await getUsers();
+  console.log(users);
 };
 
 module.exports = {
   client,
   createTables,
   seedData,
+  getUsers
 };
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const db = require('./db');
-const { client, createTables, seedData, usersAndStories } = db;
+const { client, createTables, seedData, getUsers } = db;
 const express = require('express');
 const app = express();
+
+app.get('/api/users', async(req, res, next)=> {
+  try {
+    res.send( await getUsers());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 const port = process.env.PORT || 3000;
 
